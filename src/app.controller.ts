654ce16e51cd7667043f66afd1c3fb7ce34c44d3
dmcong <congduong@descartes.network>
2023-10-20 11:41:43 +0700

    challenge-2.2.2

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('hello')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return 'quiz-2.2.2';
   }
 }
